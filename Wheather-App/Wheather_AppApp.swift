4907d0a348dd4d176fe04762846da881db57bc32
Younis Abuzayed <younisabuzayed@gmail.com>
2026-02-03 08:21:04 +0800

    Initial Commit

---
+++ Wheather-App/Wheather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wheather_AppApp.swift
+//  Wheather-App
+//
+//  Created by Younis Abuzayed on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Wheather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
